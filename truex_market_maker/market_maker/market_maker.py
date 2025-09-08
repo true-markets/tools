205a4d768fa4ead32070b000aeb5c15dc05a5e81
spencer <spencer@truemarkets.co>
2025-09-08 10:24:44 -0400

    eng-1254 - add apache license and notices

--- truex_market_maker/market_maker/market_maker.py
+++ truex_market_maker/market_maker/market_maker.py
@@ -1,3 +1,21 @@
+# Copyright 2024 True Markets
+# Copyright 2016 BitMEX (original work)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file contains code derived from the BitMEX Sample Market Maker
+# (https://github.com/BitMEX/sample-market-maker)
+
 import asyncio
 import atexit
 import os
